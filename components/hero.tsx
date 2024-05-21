356ec21306f4951da273b17d1837a387abce79fe
eliashezron <eliashezron23@gmail.com>
2024-05-21 08:34:43 +0000

    updated links

--- components/hero.tsx
+++ components/hero.tsx
@@ -75,10 +75,10 @@ export default function Hero() {
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Empowering Developers, Igniting Innovation, and Building Solutions for Africa's Future</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://forms.gle/dfLxWsSp5hMmKmer5">REGISTER</a>
+                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://forms.gle/QibFqbEmnA3uU7KE9">REGISTER</a>
                 </div>
                 <div>
-                  <a className="btn text-white bg-red-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="https://forms.gle/fhvsGGhdYGJT9Knc8">HACK</a>
+                  <a className="btn text-white bg-red-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="https://forms.gle/oqPys2jHZw3ub2JN7">HACK</a>
                 </div>
               </div>
             </div>
--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -65,10 +65,10 @@ export default function MobileMenu() {
         >
           <ul className="px-5 py-2">
             <li>
-              <Link href="https://forms.gle/dfLxWsSp5hMmKmer5" className="flex font-medium w-full text-gray-600 hover:text-gray-900 py-2 justify-center" onClick={() => setMobileNavOpen(false)}>REGISTER</Link>
+              <Link href="https://forms.gle/QibFqbEmnA3uU7KE9" className="flex font-medium w-full text-gray-600 hover:text-gray-900 py-2 justify-center" onClick={() => setMobileNavOpen(false)}>REGISTER</Link>
             </li>
             <li>
-              <Link href="https://forms.gle/hC3VHqaJx6S3FpKG9" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 w-full my-2" onClick={() => setMobileNavOpen(false)}>
+              <Link href="https://forms.gle/a4P8BBXF21JmzwVN7" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 w-full my-2" onClick={() => setMobileNavOpen(false)}>
                 <span>Sponsor</span>
                 <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                   <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fill="#999" fillRule="nonzero" />
