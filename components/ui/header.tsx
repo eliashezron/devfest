8c3cb574a94e1d0f0591a3c5ebee0812e59e385e
Mpiima <collinsmpiima@gmail.com>
2025-04-25 13:06:33 +0300

    changes on hero , added overlay

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -34,12 +34,16 @@ export default function Header() {
             {/* Desktop sign in links */}
             <ul className="flex grow justify-end flex-wrap items-center">
               <li>
-                <Link href="https://tally.so/r/mOVg1K" passHref target="_blank" rel="noopener noreferrer" className="font-medium hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">
+                <Link href="https://tally.so/r/mOVg1K" passHref target="_blank" rel="noopener noreferrer"
+                 className="btn btn-sm text-white bg-blue-600 hover:bg-blue-700 px-8 py-3 rounded-full text-sm shadow-sm transition ml-3">
                   SPONSOR
                 </Link>
               </li>
               <li>
-                <Link href="https://app.unlock-protocol.com/event/block-chain-developer-festival-5" passHref target="_blank" rel="noopener noreferrer" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                <Link href="https://app.unlock-protocol.com/event/block-chain-developer-festival-5" passHref target="_blank"
+                 rel="noopener noreferrer" 
+                 className="btn btn-sm text-white bg-gray-900 hover:bg-gray-800 px-8 py-3 rounded-full text-sm shadow-sm transition ml-3"
+                 >
                   <span>REGISTER</span>
                   <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
